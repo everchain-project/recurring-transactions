34d61ecf08d52bc0a621862d1bfbb76683cb7937
Joseph Reed <chicobitcoinjoe@gmail.com>
2018-09-05 19:29:08 -0700

    Move RecurringPayment.sol to examples

--- contracts/examples/RecurringPayment.sol
+++ contracts/examples/RecurringPayment.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.23;
 
-import "./external/Owned.sol";
-import "./external/CloneFactory.sol";
-import "./PaymentDelegate.sol";
-import "./AlarmClock.sol";
-import "./Interfaces.sol";
+import "../external/Owned.sol";
+import "../external/CloneFactory.sol";
+import "../RecurringAlarmClock.sol";
+import "../PaymentDelegate.sol";
+import "../Interfaces.sol";
 
 contract RecurringPayment is IFuturePayment, ITask, Owned {
     
@@ -18,7 +18,7 @@ contract RecurringPayment is IFuturePayment, ITask, Owned {
     uint public amount;
     
     function initialize (
-        IAlarmClock alarmClock,
+        IRecurringAlarmClock alarmClock,
         IPaymentDelegate _delegate,
         IDelegatedWallet _wallet,
         address _token,
@@ -48,30 +48,38 @@ contract RecurringPaymentFactory is CloneFactory {
     
     using PaymentDelegateLib for PaymentDelegate;
     
-    address public paymentBlueprint;
+    RecurringPayment public blueprint;
+    RecurringAlarmClockFactory public factory;
 
-    function create (
-        AlarmClockFactory alarmClockfactory,
-        IPaymentDelegate paymentDelegate,
+    constructor (
+        RecurringPayment _blueprint,
+        RecurringAlarmClockFactory _factory
+    ) public {
+        blueprint = _blueprint;
+        factory = _factory;
+    }
+
+    function createRecurringPayment (
         IDelegatedWallet delegatedWallet,
-        address alarmToken,
+        IPaymentDelegate paymentDelegate,
+        address feeRecipient,
         address transferToken,
+        address alarmToken,
         address recipient,
-        uint amount,
-        uint[10] alarmOptions,
-        uint alarmDeposit,
-        uint maxIntervals
-    ) public returns (RecurringPayment recurringPayment) {
-        recurringPayment = RecurringPayment(createClone(paymentBlueprint));
-
-        AlarmClock alarmClock = alarmClockfactory.createAlarmClock(
-            paymentDelegate,
-            delegatedWallet,
+        uint[3] racOptions,
+        uint[10] eacOptions,
+        uint paymentAmount
+    ) public returns (RecurringPayment) {
+        RecurringPayment recurringPayment = RecurringPayment(createClone(blueprint));
+        
+        RecurringAlarmClock alarmClock = factory.createRecurringAlarmClock(
             recurringPayment,
+            delegatedWallet,
+            paymentDelegate,
+            feeRecipient,
             alarmToken,
-            alarmOptions,
-            alarmDeposit,
-            maxIntervals
+            racOptions,
+            eacOptions
         );
         
         recurringPayment.initialize(
@@ -80,7 +88,7 @@ contract RecurringPaymentFactory is CloneFactory {
             delegatedWallet,    // wallet to pull alarm funds from
             transferToken,      // the token to use when making a payment
             recipient,          // the recipient of the payment
-            amount              // how much of the token to send each payment
+            paymentAmount       // how much of the token to send each payment
         );
 
         PaymentDelegate(paymentDelegate).validate(alarmClock);
