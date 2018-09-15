57035a42bb5954e031dcc34002a560590c2dbadd
Joseph Reed <chicobitcoinjoe@gmail.com>
2018-09-14 18:54:47 -0700

    updated truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,34 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
-    kovan: {
-      host: "localhost",
-      port: 8545,
-      network_id: 42,
-      gas: 4000000
-    },
-    live: {
-        host: "127.0.0.1",
-        port: 8545,
-        network_id: 1,        // Ethereum public network
-        // optional config values:
-        // gas
-        // gasPrice
-        // from - default address to use for any transaction Truffle makes during migrations
-        // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
-        //          - function that returns a web3 provider instance (see below.)
-        //          - if specified, host and port are ignored.
-      }
-  }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // for more about customizing your Truffle configuration!
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*", // Match any network id
+            from: '0x627306090abab3a6e1400e9345bc60c78a8bef57',
+            gas: 4700000,
+            gasPrice: 10000000000,
+        },
+        kovan: {
+            host: "localhost",
+            port: 8545,
+            network_id: 42,
+            from: '0x96a15a5de1727d4ed36bd3129becb09cef62ee46',
+            gas: 4000000,
+            gasPrice: 10000000000
+        },
+        live: {
+            host: "localhost",
+            port: 8545,
+            network_id: 1,        // Ethereum public network
+            // optional config values:
+            // gas
+            // gasPrice
+            // from - default address to use for any transaction Truffle makes during migrations
+            // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+            //          - function that returns a web3 provider instance (see below.)
+            //          - if specified, host and port are ignored.
+        }
+    }
 };
